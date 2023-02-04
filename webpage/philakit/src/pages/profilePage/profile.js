871d8c6824551be218577044a9b4c5b73c0bd0ab
Hallie Faust <hfaust@usc.edu>
2023-02-04 15:59:55 -0800

    login vs. register pages

--- webpage/philakit/src/pages/profilePage/profile.js
+++ webpage/philakit/src/pages/profilePage/profile.js
@@ -6,7 +6,12 @@ export default function ProfilePage() {
     return (
         <div>
             <h1>Profile Page!</h1>
-            <Link exact="true" to="/login" className="link">Login/Sign up!</Link>
+            <Link exact="true" to="/register" className="link">
+                <button>Create account</button>
+            </Link>
+            <Link exact="true" to="/login" className="link">
+                <button>Sign in</button>
+            </Link>
         </div>
     )
 }
