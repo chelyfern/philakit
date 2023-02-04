b1c12be837e7087ee9dd3d999f38b710e06fdae6
Tammy Truong <tammytru000@gmail.com>
2023-02-03 22:57:15 -0800

    renamed file name to webpage and updated manifest.json

--- webpage/philakit/src/pages/App/App.js
+++ webpage/philakit/src/pages/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {  } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage } from '../../pages'
+import { HomePage } from '..'
 import { Navbar } from '../../components'
 
 function App() {
