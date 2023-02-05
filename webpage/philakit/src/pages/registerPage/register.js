73aa558c4f1d55fe79288dec8ea27f31929f2650
ChelyFernandezUSC <cmfernan@usc.edu>
2023-02-04 16:53:35 -0800

    firebase auth

--- webpage/philakit/src/pages/registerPage/register.js
+++ webpage/philakit/src/pages/registerPage/register.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import { NavLink, useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase'
 import './register.css'
 
-export default function RegisterPage() {
+const RegisterPage = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/login")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+        });
+   
+    }
     return (
         <div>
             <h1>Sign up page</h1>
@@ -9,17 +35,35 @@ export default function RegisterPage() {
                 <form>
                     <div className="input">
                         <label>Username</label>
-                        <input type="text" name="uname" required/>
+                        <input type="email"
+                                label="Email address"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}  
+                                required                                    
+                                placeholder="Email address"                                
+                            />
                     </div>
                     <div className="input">
                         <label>Password</label>
-                        <input type="password" name="pass" required/>
+                        <input type="password"
+                                label="Create password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required                                 
+                                placeholder="Password"              
+                            />
                     </div>
                     <div className="buttons">
-                        <button>Create account</button>
+                        <button
+                            type="submit" 
+                            onClick={onSubmit}                        
+                        >  
+                        Create account</button>
                     </div>
                 </form>
             </div>
         </div>
     )
 }
+
+export default RegisterPage
