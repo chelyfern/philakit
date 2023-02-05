414c602d213da74b363d3411f6fae638b33f4830
ChelyFernandezUSC <cmfernan@usc.edu>
2023-02-04 16:23:59 -0800

    firebase and react dom 6 changes

--- webpage/philakit/src/pages/App/App.js
+++ webpage/philakit/src/pages/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {  } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Routes, BrowserRouter } from 'react-router-dom';
 import { HomePage, MapPage, BlogPage, ProfilePage, LoginPage } from '..'
 import { Navbar } from '../../components'
 
@@ -11,28 +11,17 @@ function App() {
       <div>
         <Navbar/>
       </div>
+    
+      <Routes>
+        <Route exact path="/" element={<HomePage />}/>
+        <Route exact path="/map" element={<MapPage />}/>
+          
+        <Route exact path="/blog" element={<BlogPage />}/>
+        <Route exact path="/profile" element={<ProfilePage />}/>
+        <Route exact path="/login" element={<LoginPage />}/>
 
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route exact path="/map">
-          <MapPage />
-        </Route>
-        <Route exact path="/blog">
-          <BlogPage />
-        </Route>
-        <Route exact path="/profile">
-          <ProfilePage />
-        </Route>
-        <Route exact path="/login">
-          <LoginPage />
-          
-        </Route>
-        <Route exact path="/profile">
-          
-        </Route>
-      </Switch>
+      </Routes>
+    
 
     </div>
   );
--- webpage/philakit/src/pages/index.js
+++ webpage/philakit/src/pages/index.js
@@ -1,6 +1,23 @@
+import React from 'react';
+import HomePage from './Home-Page/home-page';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 export { default as App } from './App/App.js'
 export { default as HomePage } from './Home-Page/home-page'
 export { default as MapPage } from './mapPage/map'
 export { default as BlogPage } from './blogPage/blog'
 export { default as ProfilePage } from './profilePage/profile'
 export { default as LoginPage } from './loginPage/login'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <HomePage /> }>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
