b509bf35eacf857422a5b2e28b22322bf988d7fd
ChelyFernandezUSC <cmfernan@usc.edu>
2023-02-04 17:07:55 -0800

    logout button

--- webpage/philakit/src/pages/Home-Page/home-page.js
+++ webpage/philakit/src/pages/Home-Page/home-page.js
@@ -1,8 +1,23 @@
 import React, { Link } from "react";
+import {  signOut } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 import './styles.css'
 import { SearchBar, Discover, Res } from "../../components";
 
-export default function HomePage() {
+const HomePage = () => {
+    const navigate = useNavigate();
+ 
+    const handleLogout = () => {               
+        signOut(auth).then(() => {
+        // Sign-out successful.
+            navigate("/");
+            console.log("Signed out successfully")
+        }).catch((error) => {
+        // An error happened.
+        });
+    }
+
     return (
         <div id="homepage-container">
 
@@ -13,8 +28,17 @@ export default function HomePage() {
             <div id="discover-container">
                 <Discover id="discover-section"/>
             </div>
+
+            <div>
+                <button onClick={handleLogout}>
+                    Logout
+                </button>
+
+            </div>
             <Res/>
 
         </div>
     )
 }
+
+export default HomePage
