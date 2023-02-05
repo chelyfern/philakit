5d7cde896d2dc5c2ea2ef22dd5945bad6c242197
ChelyFernandezUSC <cmfernan@usc.edu>
2023-02-04 19:05:26 -0800

    extension url saved, firebase

--- chrome-app-pk/src/App.tsx
+++ chrome-app-pk/src/App.tsx
@@ -1,9 +1,15 @@
-
+import React, {Component} from 'react';
 function App() {
+  function btnClick(): void{
+    window.open('https://philakit.surge.sh');
+  }
   return (
     <div>
       <header>
        <h4> Welcome to philakit </h4>
+       <div>
+            <button onClick={btnClick}>Go to philakit</button>
+        </div>
       </header>
     </div>
   );
